65cee64ccf23e6893fa1c9f9305dd7435a1d2978
nitin-agam <nitinagam17@gmail.com>
2022-12-11 14:10:38 +0530

    Project setup and resources added

---
+++ SwiftUI-Honeymoon/SwiftUI-Honeymoon/SwiftUI_HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HoneymoonApp.swift
+//  SwiftUI-Honeymoon
+//
+//  Created by Nitin Aggarwal on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
